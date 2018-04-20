f254f35bfe7fcc77b1951f694daa8153c6e6c988
Santiago Palladino <spalladino@gmail.com>
2018-04-20 10:28:09 -0300

    Evaluate using commander for defining each script

--- src/entrypoint.js
+++ src/entrypoint.js
@@ -6,15 +6,26 @@ const program = require('commander')
 
 module.exports = function(cb) {
 
+  // Extend commander to accept a script option
+  const context = this;
+  program.Command.prototype.script = function(fn) {
+    this.action(function() {
+      const options = arguments[arguments.length - 1];
+      options.from = options.parent.from;
+      options.network = options.parent.network || web3.eth.accounts[0];
+      fn.apply(context, arguments).then(cb).catch(cb);
+    })
+  };
+  
+  // Register global options
   program
+    .version(require('../package.json').version)
     .option('--network [network]', 'Truffle network')
-    .option('--from [from]', 'Sender')
-    .parse(process.argv)
+    .option('--from [from]', 'Sender account (required for all blockchain operations)')
+    
+  // Register each script individually
+  require('./scripts/init').register(program)
 
-  const script = `./scripts/${program.args[2]}.js`
-  const args = program.args.slice(3)
-  require(script)(...args, {
-    network: program.network,
-    from: program.from || web3.eth.accounts[0]
-  }).then(cb).catch(cb)
+  // Run program
+  program.parse(process.argv.slice(2))
 }
