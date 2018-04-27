5aeabb0c84cc3c4c32f5d42c8d7272620e2261de
Santiago Palladino <spalladino@gmail.com>
2018-04-27 12:01:20 -0300

    Set global objects in runWithTruffle

--- src/utils/runWithTruffle.js
+++ src/utils/runWithTruffle.js
@@ -2,14 +2,14 @@ const Web3 = require('web3')
 const TruffleConfig = require("truffle-config");
 const TruffleEnvironment = require("truffle-core/lib/environment");
 
-export default function runWithTruffle(script) {
-  if(!scriptPath) throw 'A script path must be given'
-  const config = TruffleConfig.detect(options);
+export default function runWithTruffle(script, network) {
+  const options = { logger: console }
+  const config = TruffleConfig.detect(options)
+  config.network = network;
   TruffleEnvironment.detect(config, function(error) {
     if (error) throw error
     global.web3 = new Web3(config.provider)
-    // global.artifacts = config.artifacts?
-    // TODO: inject artifacts globally
+    global.artifacts = config.resolver
     script()
   })
 }
