9c2569f5619b4664c36fa2e79c06058f9b9d9a3a
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-04-27 17:13:27 -0300

    Entrypoints for vouching commands

---
+++ src/commands/vouching/unvouch.js
@@ -0,0 +1,17 @@
+const unvouch = require('../../scripts/unvouch')
+const runWithTruffle = require('../../utils/runWithTruffle')
+
+module.exports = function(program) {
+  program
+    .command('unvouch <release> <amount>')
+    .description("Unvouches a requested amount of ZEP tokens to a given release.\n  " +
+      "Provide the <release> address to unvouch for.\n  " +
+      "Provide the raw-amount of ZEP tokens to be unvouched for the given <release>")
+    .usage('<release> <amount> --network <network>')
+    .option('-f, --from <from>', 'Set the transactions sender')
+    .option('-n, --network <network>', 'Provide a network to be used')
+    .action(function (release, amount, options) {
+      const { network, from } = options
+      runWithTruffle(() => unvouch(release, amount, { network, from }), network)
+    })
+}
---
+++ src/commands/vouching/vouch.js
@@ -0,0 +1,17 @@
+const vouch = require('../../scripts/vouch')
+const runWithTruffle = require('../../utils/runWithTruffle')
+
+module.exports = function(program) {
+  program
+    .command('vouch <release> <amount>')
+    .description("Vouches a requested amount of ZEP tokens to a given release.\n  " +
+      "Provide the <release> address to vouch for.\n  " +
+      "Provide the raw-amount of ZEP tokens to be vouched for the given <release>")
+    .usage('<release> <amount> --network <network>')
+    .option('-f, --from <from>', 'Set the transactions sender')
+    .option('-n, --network <network>', 'Provide a network to be used')
+    .action(function (release, amount, options) {
+      const { network, from } = options
+      runWithTruffle(() => vouch(release, amount, { network, from }), network)
+    })
+}
