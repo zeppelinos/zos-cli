c126bba0e59f2281873c925c4d90d4f1cf9c9890
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-05-01 18:41:23 -0300

    Model stdlib deployer

---
+++ src/zos-lib/stdlib/StdlibDeployer.js
@@ -0,0 +1,58 @@
+import fs from 'fs'
+import Logger from '../../utils/Logger'
+import ContractsProvider from "../../models/ContractsProvider"
+
+const log = new Logger('StdlibFactory')
+
+export default class StdlibDeployer {
+  constructor(owner) {
+    this.owner = owner
+    this.txParams = { from: owner }
+  }
+
+  async call(name) {
+    this._parseJsonData(name)
+    await this._createContractDirectory()
+    await this._deployContracts(name)
+    return this.contractDirectory.address
+  }
+
+  async _createContractDirectory() {
+    log.info('Deploying new ContractDirectory...')
+    const ContractDirectory = ContractsProvider.getByName('ContractDirectory')
+    this.contractDirectory = await ContractDirectory.new(this.txParams)
+    log.info(`Deployed ContractDirectory ${this.contractDirectory.address}`)
+  }
+
+  async _deployContracts(name) {
+    await Promise.all(this._contractsList().map(async (contractName) => {
+      const deployed = await this._deployContract(name, contractName)
+      await this.contractDirectory.setImplementation(contractName, deployed.address, this.txParams)
+    }))
+  }
+
+  async _deployContract(name, contractName) {
+    log.info(`Deploying new ${contractName}...`)
+    const contractClass = await this._getContractClass(name, contractName)
+    const contract = await contractClass.new(this.txParams)
+    log.info(`Deployed ${contractName} ${contract.address}`)
+    return contract
+  }
+
+  async _getContractClass(name, contractName) {
+    const implementationName = this.jsonData.contracts[contractName]
+    if (!implementationName) throw `Contract ${contractName} not found in package`
+    const contractSchema = `node_modules/${name}/build/contracts/${implementationName}.json`;
+    const data = JSON.parse(fs.readFileSync(contractSchema))
+    return ContractsProvider.getByJSONData(data)
+  }
+  
+  _contractsList() {
+    return Object.keys(this.jsonData.contracts)
+  }
+  
+  _parseJsonData(name) {
+    const filename = `node_modules/${name}/package.zos.json`
+    this.jsonData = JSON.parse(fs.readFileSync(filename))
+  }
+}
