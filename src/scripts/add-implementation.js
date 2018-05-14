c34638c02c0fd34773c425e689f96d79b28ac6c9
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-05-14 13:48:25 -0300

    Support adding all compiled implementations

--- src/scripts/add-implementation.js
+++ src/scripts/add-implementation.js
@@ -4,10 +4,9 @@ export default function addImplementation({ contractsData, packageFileName = und
   if (contractsData.length === 0) throw new Error('At least one contract name must be provided to add a new implementation.')
 
   const appController = new AppController(packageFileName)
-
   contractsData.forEach(({ name, alias }) => {
     appController.validateImplementation(name)
-    appController.addImplementation(alias, name)
-    appController.writePackage()
+    appController.addImplementation(alias || name, name)
   })
+  appController.writePackage()
 }
