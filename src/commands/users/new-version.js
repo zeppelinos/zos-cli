32799c6592ae0633b197229f397d85832d647b37
Santiago Palladino <spalladino@gmail.com>
2018-05-15 15:42:37 -0300

    Remove short opts for readability
    Removes the short version for opts that are not that frequent, in
    order to improve readability of the commands used and avoid potential
    mistakes from the users.
    
    Removed the following short opts:
    - -s for --stdlib
    - -i for --init
    - -a for --args

--- src/commands/users/new-version.js
+++ src/commands/users/new-version.js
@@ -6,7 +6,7 @@ module.exports = function(program) {
     .command('new-version <version>')
     .usage('<version> [options]')
     .description("Bump a new <version> of your project with zeppelin_os.")
-    .option('-s, --stdlib <stdlib>', 'Standard library to use')
+    .option('--stdlib <stdlib>', 'Standard library to use')
     .option('--no-install', 'Skip installing stdlib npm dependencies')
     .option('--sync <network>', 'Sync your project with the blockchain')
     .option('-f, --from <from>', 'Set the transactions sender in case you run with --sync')
