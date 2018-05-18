f602806366e63c44f49d46bb8aad4e741e8ed173
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-05-18 12:20:24 -0300

    Install zos-lib@0.1.22

--- test/helpers/json.js
+++
@@ -1,8 +0,0 @@
-import { FileSystem as fs } from 'zos-lib';
-
-// TODO: Use new version of FileSystem including this function
-export function editJson(file, cb) {
-  const data = fs.parseJson(file);
-  cb(data);
-  fs.writeJson(file, data);
-}
