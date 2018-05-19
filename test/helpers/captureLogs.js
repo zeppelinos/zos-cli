f55886a1f74604c139617b4c89f52d27293ab9ac
Santiago Palladino <spalladino@gmail.com>
2018-05-19 16:03:59 -0300

    Remove test logger in favour of captureLogs
    TestLogger was only used in status command, and only captured the
    logs specific to that command, missing all the logging from lib.

--- test/helpers/captureLogs.js
+++ test/helpers/captureLogs.js
@@ -14,4 +14,16 @@ export default class CaptureLogs {
     Logger.prototype.error = this.origError;
     Logger.prototype.info = this.origInfo;
   }
+
+  match(re) {
+    return _(_.concat(this.infos, this.errors)).map((msg) => msg.match(re)).compact().head();
+  }
+
+  get text() {
+    return this.toString();
+  }
+
+  toString() {
+    return _.concat(this.infos, this.errors).join("\n");
+  }
 }
