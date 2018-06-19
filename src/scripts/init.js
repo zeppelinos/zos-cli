75fd9dff077957f57fa394b82dfd9ee8539fadb4
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-06-19 19:13:16 -0300

    Fix init commands

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -1,6 +1,7 @@
 import LocalAppController from  '../models/local/LocalAppController'
+import ZosPackageFile from "../models/files/ZosPackageFile";
 
-export default async function init({ name, version, stdlibNameVersion = undefined, installLib = false, force = false, packageFile = undefined }) {
+export default async function init({ name, version, stdlibNameVersion = undefined, installLib = false, force = false, packageFile = new ZosPackageFile() }) {
   if (name === undefined) throw Error('A project name must be provided to initialize the project.')
   
   const controller = new LocalAppController(packageFile)
