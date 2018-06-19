3a2ebbd5a810fb7aef4ddbff3767192000ab70ac
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-06-19 19:13:16 -0300

    Fix init commands

--- src/scripts/init-lib.js
+++ src/scripts/init-lib.js
@@ -1,6 +1,6 @@
 import LocalLibController from  '../models/local/LocalLibController'
 
-export default async function initLib({ name, version, force = false, packageFile = undefined }) {
+export default async function initLib({ name, version, force = false, packageFile = new ZosPackageFile() }) {
   if (name === undefined) throw Error('A project name must be provided to initialize the project.')
   
   const libController = new LocalLibController(packageFile)
