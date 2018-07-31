6590ec2911241029a9363cf43ba02eb985a8638b
jota <juanbanelli@gmail.com>
2018-07-31 18:13:17 -0300

    Change some test variables management

--- test/scripts/verify.test.js
+++ test/scripts/verify.test.js
@@ -12,16 +12,10 @@ import push from '../../src/scripts/push.js'
 import ZosPackageFile from '../../src/models/files/ZosPackageFile'
 import ZosNetworkFileFile from '../../src/models/files/ZosNetworkFile'
 
-const ImplV1 = Contracts.getFromLocal('ImplV1')
-
 contract('verify script', function () {
   const contractAlias = 'Impl'
   const network = 'test'
   const txParams = {}
-  let packageFile
-  let networkFile
-  let logs
-  let axiosStub
 
   const assertVerify = async function (contractAlias, options, message) {
     try {
@@ -34,14 +28,14 @@ contract('verify script', function () {
   describe('validations', function () {
     describe('with invalid package or network files', function () {
       it('throws error if zOS project is not yet initialized', async function () {
-        packageFile = new ZosPackageFile('non-existent-package.zos.json')
-        networkFile = packageFile.networkFile(network)
+        const packageFile = new ZosPackageFile('non-existent-package.zos.json')
+        const networkFile = packageFile.networkFile(network)
         await assertVerify(contractAlias, { network, networkFile }, /Run 'zos init' first to initialize the project./)
       })
 
       it('throws error if contract not yet added', async function () {
-        packageFile = new ZosPackageFile('test/mocks/packages/package-with-contracts.zos.json')
-        networkFile = packageFile.networkFile(network)
+        const packageFile = new ZosPackageFile('test/mocks/packages/package-with-contracts.zos.json')
+        const networkFile = packageFile.networkFile(network)
         const nonExistentContract = 'NonExistent'
         await assertVerify(nonExistentContract, { network, networkFile }, /not found in this project/)
       })
@@ -49,55 +43,53 @@ contract('verify script', function () {
 
     describe('with valid package and network files', async function () {
       beforeEach(function () {
-        packageFile = new ZosPackageFile('test/mocks/packages/package-with-contracts.zos.json')
-        networkFile = packageFile.networkFile(network)
+        this.packageFile = new ZosPackageFile('test/mocks/packages/package-with-contracts.zos.json')
+        this.networkFile = this.packageFile.networkFile(network)
       })
 
       it('throws error if contract not yet deployed', async function () {
-        await assertVerify(contractAlias, { network, networkFile }, /is not deployed to/)
+        await assertVerify(contractAlias, { network, networkFile: this.networkFile }, /is not deployed to/)
       })
 
       it('throws error if contract source code has changed locally since last deploy', async function () {
-        await push({ network, networkFile, txParams })
-        const contracts = networkFile.contracts
+        await push({ network, networkFile: this.networkFile, txParams })
+        const contracts = this.networkFile.contracts
         contracts[contractAlias].bytecodeHash = '0x0303456'
-        networkFile.contracts = contracts
-        await assertVerify(contractAlias, { network, networkFile }, /has changed locally since the last deploy/)
+        this.networkFile.contracts = contracts
+        await assertVerify(contractAlias, { network, networkFile: this.networkFile }, /has changed locally since the last deploy/)
       })
     })
   })
 
   describe('contract verification', function () {
     beforeEach(async function () {
-      packageFile = new ZosPackageFile('test/mocks/packages/package-with-contracts.zos.json')
-      networkFile = packageFile.networkFile(network)
-      await push({ network, networkFile, txParams })
-      const contracts = networkFile.contracts
-      networkFile.contracts = contracts
-      logs = new CaptureLogs()
-      axiosStub = sinon.stub(axios, 'request')
+      this.packageFile = new ZosPackageFile('test/mocks/packages/package-with-contracts.zos.json')
+      this.networkFile = this.packageFile.networkFile(network)
+      await push({ network, networkFile: this.networkFile, txParams })
+      this.logs = new CaptureLogs()
+      this.axiosStub = sinon.stub(axios, 'request')
     })
 
     afterEach(function () {
-      logs.restore()
-      axiosStub.restore()
+      this.logs.restore()
+      this.axiosStub.restore()
     })
 
     it('throws error if specifying not permitted remote', async function () {
-      await assertVerify(contractAlias, { network, networkFile, remote: 'invalid-remote' }, /Invalid remote/)
+      await assertVerify(contractAlias, { network, networkFile: this.networkFile, remote: 'invalid-remote' }, /Invalid remote/)
     })
 
     it('throws error if contract could not be verified', async function () {
-      axiosStub.returns({ status: 200, data: '<div id="infoModal"><div class="modal-body"> Error: </div></div>' })
-      await assertVerify(contractAlias, { network, networkFile, remote: 'etherchain' }, /Error/)
+      this.axiosStub.returns({ status: 200, data: '<div id="infoModal"><div class="modal-body"> Error: </div></div>' })
+      await assertVerify(contractAlias, { network, networkFile: this.networkFile, remote: 'etherchain' }, /Error/)
     })
 
     it('logs a success info message when contract is verified', async function () {
-      axiosStub.returns({ status: 200, data: '<div id="infoModal"><div class="modal-body"> successful </div></div>' })
-      await verify(contractAlias, { network, networkFile, remote: 'etherchain' })
-      logs.infos.should.have.lengthOf(2)
-      logs.infos[0].should.match(/Verifying and publishing/)
-      logs.infos[1].should.match(/Contract verified and published successfully. You can check it here/)
+      this.axiosStub.returns({ status: 200, data: '<div id="infoModal"><div class="modal-body"> successful </div></div>' })
+      await verify(contractAlias, { network, networkFile: this.networkFile, remote: 'etherchain' })
+      this.logs.infos.should.have.lengthOf(2)
+      this.logs.infos[0].should.match(/Verifying and publishing/)
+      this.logs.infos[1].should.match(/Contract verified and published successfully. You can check it here/)
     })
   })
 })
