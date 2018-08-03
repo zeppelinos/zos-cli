f9f7b32b3796f9df215a0689fdedbc47246d191b
bakaoh <tatattai@gmail.com>
2018-08-03 09:57:02 +0700

    Add all commands test

--- test/commands/add.test.js
+++ test/commands/add.test.js
@@ -1,22 +1,22 @@
 'use strict'
 require('../setup')
 
-import {setup, itShouldParse} from './share';
+import {stubCommands, itShouldParse} from './share';
 
 contract('add command', function() {
 
-  setup()
+  stubCommands()
 
-  itShouldParse('should call add script with an alias and a filename', 'add', ['node', 'zos', 'add', 'ImplV1:Impl', '--skip-compile'], function(fake) {
-    fake.should.have.been.calledWithExactly({ contractsData: [ { name: 'ImplV1', alias: 'Impl' } ] })
+  itShouldParse('should call add script with an alias and a filename', 'add', 'zos add ImplV1:Impl --skip-compile', function(add) {
+    add.should.have.been.calledWithExactly({ contractsData: [ { name: 'ImplV1', alias: 'Impl' } ] })
   })
   
-  itShouldParse('should call add-all script when passing --all option', 'addAll', ['node', 'zos', 'add', '--all', '--skip-compile'], function(fake) {
-    fake.should.have.been.calledWithExactly( { } )
+  itShouldParse('should call add-all script when passing --all option', 'addAll', 'zos add --all --skip-compile', function(addAll) {
+    addAll.should.have.been.calledWithExactly( { } )
   })
 
-  itShouldParse('should call push script when passing --push option', 'push', ['node', 'zos', 'add', '--all', '--push', 'test', '--skip-compile'], function(fake) {
-    fake.should.have.been.calledWithExactly({ deployStdlib: undefined, reupload: undefined, network: 'test', txParams: {} })
+  itShouldParse('should call push script when passing --push option', 'push', 'zos add --all --push test --skip-compile', function(push) {
+    push.should.have.been.calledWithExactly({ deployStdlib: undefined, reupload: undefined, network: 'test', txParams: {} })
   })
 
 })
